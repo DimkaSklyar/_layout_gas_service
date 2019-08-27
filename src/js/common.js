5b6060b42d0146e38354fa8a1ca587d20f2aaa5c
Dmitry <30568388+DimkaSklyar@users.noreply.github.com>
2019-08-27 21:06:23 +0300

    beta 1

--- src/js/common.js
+++ src/js/common.js
@@ -38,6 +38,15 @@ $(document).ready(function () {
           dots: true,
           arrows: false
         }
+      },
+      {
+        breakpoint: 577,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          dots: true,
+          arrows: false
+        },
       }
       // You can unslick at a given breakpoint now by adding:
       // settings: "unslick"
@@ -82,6 +91,44 @@ $(document).ready(function () {
     ]
   });
 
+  $('.brand__carousel_service').slick({
+    dots: true,
+    infinite: false,
+    speed: 300,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    arrows: false,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ]
+  });
+
+
   $('.popup-youtube, .popup-vimeo, .popup-gmaps').magnificPopup({
 		disableOn: 700,
 		type: 'iframe',
@@ -113,6 +160,47 @@ $(document).ready(function () {
     $(".header-bottom__menu").fadeToggle("slow", "linear" );
   });
 
-
+  $('.menu').addClass('original').
+  clone().
+  insertAfter('.menu').
+  addClass('cloned').
+  css('position','fixed').
+  css('top','0').
+  css('margin-top','0').
+  css('z-index','500').
+  css('width','100%').
+  css('padding','10px').
+  removeClass('original').
+  hide();
+  
+  scrollIntervalID = setInterval(stickIt, 10);
+  
+  function stickIt() {
+  
+    var orgElementPos = $('.original').offset();
+    orgElementTop = orgElementPos.top;               
+  
+    if ($(window).scrollTop() >= (orgElementTop)) {
+      // scrolled past the original position; now only show the cloned, sticky element.
+      // Cloned element should always have same left position and width as original element.     
+      orgElement = $('.original');
+      coordsOrgElement = orgElement.offset();
+      leftOrgElement = coordsOrgElement.left;  
+      widthOrgElement = orgElement.css('width');
+      
+      $('.cloned').
+      css('left', leftOrgElement + 'px').
+      css('top', 0).
+      css('width', widthOrgElement + 'px')
+      .show();
+      
+      $('.original').css('visibility','hidden');
+    }
+    else {
+      // not scrolled past the menu; only show the original menu.
+      $('.cloned').hide();
+      $('.original').css('visibility', 'visible');
+    }
+  }
 
 });
